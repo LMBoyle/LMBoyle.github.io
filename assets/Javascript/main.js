e86847055ac724960230356517f10196844d07b4
LMBoyle <boylelukem@gmail.com>
2019-08-29 20:13:23 -0400

    added dropdown to nav

--- assets/Javascript/main.js
+++ assets/Javascript/main.js
@@ -1,11 +1,4 @@
 $( document ).ready(function) {
-  $(".dropdown-trigger").dropdown();
 
-  document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.tooltipped');
-    var instances = M.Tooltip.init(elems, options);
-
-    
-  });
 
 }
