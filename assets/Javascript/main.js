b027f9b8cd183fc884f9199561fbb490f3ba2a43
LMBoyle <boylelukem@gmail.com>
2019-09-01 13:25:21 -0400

    added to function to toggle class of dot based on what edu/work is showing

--- assets/Javascript/main.js
+++ assets/Javascript/main.js
@@ -210,29 +210,55 @@ var workNum = 0;
     switch (eduNum) {
       case (0):
         $(".eduArrow").attr("style", education[0].arrow);
+        $("#eduState").addClass("dotSelected");
+        $("#eduElon").removeClass("dotSelected");
+        $("#eduPT").removeClass("dotSelected");
+        $("#eduCode").removeClass("dotSelected");
         break;
       case (1):
         $(".eduArrow").attr("style", education[1].arrow);
+        $("#eduElon").addClass("dotSelected");
+        $("#eduState").removeClass("dotSelected");
+        $("#eduPT").removeClass("dotSelected");
+        $("#eduCode").removeClass("dotSelected");
         break;
       case (2):
         $(".eduArrow").attr("style", education[2].arrow);
+        $("#eduPT").addClass("dotSelected");
+        $("#eduState").removeClass("dotSelected");
+        $("#eduElon").removeClass("dotSelected");
+        $("#eduCode").removeClass("dotSelected");
         break;
       case (3):
         $(".eduArrow").attr("style", education[3].arrow)
+        $("#eduCode").addClass("dotSelected");
+        $("#eduState").removeClass("dotSelected");
+        $("#eduElon").removeClass("dotSelected");
+        $("#eduPT").removeClass("dotSelected");
         break;
       default:
-        $(".eduArrow").attr("style", education[0].arrow)
+        $(".eduArrow").attr("style", education[0].arrow);
+        $("#eduState").addClass("dotSelected");
+        $("#eduElon").removeClass("dotSelected");
+        $("#eduPT").removeClass("dotSelected");
+        $("#eduCode").removeClass("dotSelected");
     }
 
     switch (workNum) {
       case (0):
         $(".workArrow").attr("style", work[0].arrow);
+        $("#workDoby").addClass("dotSelected");
+        $("#workRec").removeClass("dotSelected");
         break;
       case (1):
         $(".workArrow").attr("style", work[1].arrow);
+        $("#workRec").addClass("dotSelected");
+        $("#workDoby").removeClass("dotSelected");
         break;
       default:
-        $(".workArrow").attr("style", work[0].arrow)
+        $(".workArrow").attr("style", work[0].arrow);
+        $("#workDoby").addClass("dotSelected");
+        $("#workRec").removeClass("dotSelected");
     }
   }
 
