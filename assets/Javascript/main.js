a167d86b8f56893cc2497ca542ddf29a0526f167
LMBoyle <boylelukem@gmail.com>
2019-09-01 12:22:31 -0400

    function to move arrow based on edu

--- assets/Javascript/main.js
+++ assets/Javascript/main.js
@@ -9,43 +9,49 @@ var education = [
     name: "NC State University",
     period: "2015-2016",
     address: "Raleigh, North Carolina",
-    des: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis quis elit massa. Etiam sed dapibus nibh, id posuere nunc. Quisque eu felis odio."
+    des: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis quis elit massa. Etiam sed dapibus nibh, id posuere nunc. Quisque eu felis odio.",
+    arrow: "top: 6px",
   },
   {
     name: "Elon University",
     period: "2017-2018",
     address: "Elon, North Carolina",
-    des: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis quis elit massa. Etiam sed dapibus nibh, id posuere nunc. Quisque eu felis odio."
+    des: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis quis elit massa. Etiam sed dapibus nibh, id posuere nunc. Quisque eu felis odio.",
+    arrow: "top: 161px",
   },
   {
     name: "National Academy of Sports Medicine",
     period: "2018",
     address: "Burlington, North Carolina",
-    des: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis quis elit massa. Etiam sed dapibus nibh, id posuere nunc. Quisque eu felis odio."
+    des: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis quis elit massa. Etiam sed dapibus nibh, id posuere nunc. Quisque eu felis odio.",
+    arrow: "top: 270px",
   },
   {
     name: "The Coding Boot Campt at UNC-Chapel Hill",
     period: "2019",
     address: "Raleigh, North Carolina",
-    des: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis quis elit massa. Etiam sed dapibus nibh, id posuere nunc. Quisque eu felis odio."
+    des: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis quis elit massa. Etiam sed dapibus nibh, id posuere nunc. Quisque eu felis odio.",
+    arrow: "top: 336px",
   },
 ]
 var eduNum = 0;
 
 var work = [
   {
-    name: "Elon University Campus Recreation",
-    period: "2017-2018",
-    address: "Elon, North Carolina",
-    role: "Koury Operations Assistant",
-    des: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis quis elit massa. Etiam sed dapibus nibh, id posuere nunc. Quisque eu felis odio."
-  },
-  {
     name: "Brian Doby Shell",
     period: "2017-Present",
     address: "Burlington, North Carolina",
     role: "Shift Manager",
-    des: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis quis elit massa. Etiam sed dapibus nibh, id posuere nunc. Quisque eu felis odio."
+    des: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis quis elit massa. Etiam sed dapibus nibh, id posuere nunc. Quisque eu felis odio.",
+    arrow: "top: 85px",
+  },
+  {
+    name: "Elon University Campus Recreation",
+    period: "2017-2018",
+    address: "Elon, North Carolina",
+    role: "Koury Operations Assistant",
+    des: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis quis elit massa. Etiam sed dapibus nibh, id posuere nunc. Quisque eu felis odio.",
+    arrow: "top: 211px"
   },
 ]
 var workNum = 0;
@@ -183,7 +189,7 @@ var workNum = 0;
     var role = work[workNum].role;
     var des = work[workNum].des;
 
-    if (place === work[0].name) {
+    if (place === work[1].name) {
       $("#workPlace").text(place);
       $("#workPlace").attr("style", "font-size: 14px;")
     }
@@ -198,6 +204,25 @@ var workNum = 0;
     $("#workDes").text(des);
 
     moveArrow();
+  }
+
+  function moveArrow() {
+    switch (eduNum) {
+      case (0):
+        $(".eduArrow").attr("style", education[0].arrow);
+        break;
+      case (1):
+        $(".eduArrow").attr("style", education[1].arrow);
+        break;
+      case (2):
+        $(".eduArrow").attr("style", education[2].arrow);
+        break;
+      case (3):
+        $(".eduArrow").attr("style", education[3].arrow)
+        break;
+      default:
+        $(".eduArrow").attr("style", education[0].arrow)
+    }
   }
 
 // CALLBACK ===========================
